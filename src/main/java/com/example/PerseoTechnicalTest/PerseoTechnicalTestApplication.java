e60b105a2e1b2811524d04d84b1418983c93318f
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-09-10 14:57:04 +0200

    first commit

---
+++ src/main/java/com/example/PerseoTechnicalTest/PerseoTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.PerseoTechnicalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerseoTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerseoTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PerseoTechnicalTest/PerseoTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PerseoTechnicalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseoTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
